d59dfe8714dda51078e0316901bce52c649949d2
nujabes17 <alihasyim117@gmail.com>
2022-06-05 15:48:39 +0700

    Menambahkan router ke semua page pada navbar

---
+++ src/scripts/views/pages/berita-gempa.js
@@ -0,0 +1,13 @@
+const BeritaGempa = {
+  async render() {
+    return `
+      <h2>berita gempa page</h2>
+    `;
+  },
+ 
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
+};
+ 
+export default BeritaGempa;
