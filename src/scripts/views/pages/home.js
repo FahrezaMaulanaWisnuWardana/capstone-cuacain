918f249f4fbbf6f88d98152082c51e2a72da7193
nujabes17 <alihasyim117@gmail.com>
2022-06-08 16:22:42 +0700

    Menambahkan tampilan halaman home dan halaman about us

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,7 +1,16 @@
 const Home = {
   async render() {
     return `
-      <h2>home page</h2>
+    <div class="hero">
+      <div class="hero-text">
+          <h2>Halo, Selamat datang</p>
+          <h1>Informasi Cuaca Terkini <br> dan Data Gempa Yang Akurat</h1>
+          <p>Memiliki informasi berbagai cuaca dan data gempa lengkap dengan mudah dan nyaman</p>
+      </div>
+      <div id="maincontent">
+      
+      </div>
+    </div>
     `;
   },
  
